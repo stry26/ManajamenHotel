145c2dc61a45b00906741c2ce1b8098ce2266cf7
stry26 <km440685@gmail.com>
2026-07-14 15:51:44 +0700

    mongodb setup

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import VueSweetalert2 from 'vue-sweetalert2';
+import Swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp(App);
 
 app.use(router);
-app.use(VueSweetalert2);
+app.config.globalProperties.$swal = Swal;
 
 app.mount('#app');
