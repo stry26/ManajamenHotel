077eeefb1f9f6fdeffee876f53bc347aec63a3f4
stry26 <km440685@gmail.com>
2026-07-14 20:08:32 +0700

    persiapan vercel

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.use('/api/dashboard', dashboardRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
